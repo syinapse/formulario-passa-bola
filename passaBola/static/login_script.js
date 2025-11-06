ed9abc47ab6a75324d376265fb0540db256c02a1
Scacchetti <luiscacchetti07@gmail.com>
2025-11-06 00:27:31 -0300

    feat: tootltip com o login teste

--- passaBola/static/login_script.js
+++ passaBola/static/login_script.js
@@ -1,5 +1,7 @@
 const chkPassword = document.getElementById('isPasswordVisible');
 const inputPassword = document.getElementById('inputPassword');
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 chkPassword.addEventListener('change', function() {
   if (this.checked)
